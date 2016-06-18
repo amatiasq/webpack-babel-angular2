c5f15a4e1abacae49b0cff6d8fe3249e4af3e335
A. Matías Quezada <amatiasq@gmail.com>
2016-06-18 13:22:20 +0200

    Added html & less. Fixed eslint error

--- app/application.js
+++ app/application.js
@@ -3,7 +3,7 @@ import { Component as component } from '@angular/core';
 
 @component({
   selector: 'my-app',
-  template: '<h1>My First Angular 2 Patata</h1>',
+  template,
 })
 export default class ApplicationComponent {
 
--- app/index.js
+++ app/index.js
@@ -1,4 +1,4 @@
 import { bootstrap } from '@angular/platform-browser-dynamic';
-import ApplicationComponent from './application.component';
+import ApplicationComponent from './application';
 
 bootstrap(ApplicationComponent);
